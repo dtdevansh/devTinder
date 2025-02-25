b44b7ac0cd216fc447a5c141da03517ad2a0e463
dtdevansh <dt.devansh.tyagi@gmail.com>
2025-02-25 19:15:11 +0530

    Exploring the routes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,24 @@ const express = require("express");
 const port = 7777;
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Welcome to the Dashboard");
+app.get("/user", (req, res) => {
+  res.send({ name: "Devansh Tyagi", gender: "male" });
 });
 
-app.use("/test", (req, res) => {
-  res.send("Welcome to the Test page");
+app.post("/user", (req, res) => {
+  res.send("User saved succesfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("User updated succesfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User deleted succesfully");
+});
+
+app.use("/", (req, res) => {
+  res.send("Welcome to the Dashboard");
 });
 
 app.listen(port, () => {
